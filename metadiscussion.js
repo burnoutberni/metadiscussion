f1da558b0bb74183a3b43a2cb11bba0f5ea97fc5
burnoutberni <bernhard.hayden@gmail.com>
2015-12-18 22:19:08 +0100

    made ALL the things better

--- metadiscussion.js
+++ metadiscussion.js
@@ -1,4 +1,5 @@
 Speakers = new Mongo.Collection("speakers");
+var count = 0;
 
 if (Meteor.isClient) {
   Meteor.subscribe("speakers");
@@ -6,12 +7,15 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     speakers: function () {
-      return Speakers.find({});
+      return Speakers.find({}, { sort: { count: 1 } });
     },
     isAdmin: function () {
       var userId = Meteor.userId();
       var admin = Meteor.users.findOne(userId).admin;
       return admin;
+    },
+    noSpeakers: function () {
+      return Speakers.findOne({}) ? false : true;
     }
   });
 
@@ -27,13 +31,24 @@ if (Meteor.isClient) {
   Template.speaker.helpers({
     isOwner: function () {
       return this.owner === Meteor.userId();
+    },
+    isAdmin: function () {
+      var userId = Meteor.userId();
+      var admin = Meteor.users.findOne(userId).admin;
+      return admin;
     }
   });
 
   Template.speaker.events({
     'click .delete': function () {
       Meteor.call("deleteSpeaker", this._id);
-    }
+    },
+    'click .up': function () {
+      Meteor.call("moveUp", this._id);
+    },
+    'click .down': function () {
+      Meteor.call("moveDown", this._id);
+    },
   });
 
   Accounts.ui.config({
@@ -54,16 +69,43 @@ Meteor.methods({
     Speakers.insert({
       username: Meteor.user().username,
       owner: Meteor.userId(),
+      count: count,
       createdAt: new Date()
     });
+    count++;
   },
   deleteSpeaker: function (speakerId) {
+    if (typeof Speakers.findOne(speakerId) == "undefined") {
+      throw new Meteor.Error("no-more-speakers");
+    }
     if (Speakers.findOne(speakerId).owner !== Meteor.userId() &&
         ! Meteor.user().admin) {
       throw new Meteor.Error("not-authorized");
     }
 
     Speakers.remove(speakerId);
+  },
+  moveUp: function (speakerId) {
+    if (Speakers.findOne(speakerId).owner !== Meteor.userId() &&
+        ! Meteor.user().admin) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    var speakerCount = Speakers.findOne(speakerId).count;
+    if ( Speakers.update({count: {$eq: speakerCount + 1}}, {$inc: {count: -1}}) ) {
+      Speakers.update(speakerId, {$inc: {count: 1}});
+    }
+  },
+  moveDown: function (speakerId) {
+    if (Speakers.findOne(speakerId).owner !== Meteor.userId() &&
+        ! Meteor.user().admin) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    var speakerCount = Speakers.findOne(speakerId).count;
+    if ( Speakers.update({count: {$eq: speakerCount - 1}}, {$inc: {count: +1}}) ) {
+      Speakers.update(speakerId, {$inc: {count: -1}});
+    }
   },
   next: function () {
     if (! Meteor.user().admin) {
@@ -75,12 +117,13 @@ Meteor.methods({
 });
 
 if (Meteor.isServer) {
+
   Meteor.startup(function () {
     //on server startup deleting all speakers
-    // Speakers.remove({})
+    Speakers.remove({});
 
     // make nini admin
-    Meteor.users.update({ username: 'nini' }, { $set: {admin: true} })
+    Meteor.users.update({ username: 'nini' }, { $set: {admin: true} });
   });
 
   Meteor.publish("speakers", function () {
@@ -90,7 +133,7 @@ if (Meteor.isServer) {
   Meteor.publish("userData", function () {
     if (this.userId) {
       return Meteor.users.find({_id: this.userId},
-                               {fields: {'admin': 1}});
+                               {fields: {'admin': 1, 'count': 1}});
     } else {
       this.ready();
     }

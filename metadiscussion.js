35a4e6558c5492d073b38597bbba02e5e5527bbc
burnoutberni <bernhard.hayden@gmail.com>
2015-12-19 22:56:46 +0100

    minor tweak

--- metadiscussion.js
+++ metadiscussion.js
@@ -162,7 +162,7 @@ if (Meteor.isServer) {
   Meteor.publish("userData", function () {
     if (this.userId) {
       return Meteor.users.find({_id: this.userId},
-                               {fields: {'admin': 1, 'count': 1}});
+                               {fields: {'admin': 1}});
     } else {
       this.ready();
     }
